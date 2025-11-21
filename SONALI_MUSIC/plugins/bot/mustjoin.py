894f702c396b443806728bf137f6289b2b8140e4
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2025-11-21 08:28:27 +0530

    Update mustjoin.py

--- SONALI_MUSIC/plugins/bot/mustjoin.py
+++ SONALI_MUSIC/plugins/bot/mustjoin.py
@@ -5,7 +5,7 @@ from SONALI_MUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "kriti_bot_update"
+MUST_JOIN = "kittuu_updates"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -26,8 +26,8 @@ async def must_join_channel(app: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/KRITI_SUPPORT_GROUP"),
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/kriti_bot_update"),
+                        
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/kittuu_updates"),
                             ]
                         ]
                     )
